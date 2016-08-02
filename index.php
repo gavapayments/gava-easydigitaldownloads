8b6c0206699ab0ff12b3d052d43a1e4ff7569eed
Sam <sam.takunda@gmail.com>
2016-08-02 17:44:51 +0200

    Listen for and process callbacks securely

--- index.php
+++ index.php
@@ -10,20 +10,27 @@ Author URI: http://github.com/ihatehandles
 */
 
 //Register the gateway
-function gava_edd_register_gateway($gateways){
-	$gateways['gava_edd'] = array('admin_label' => 'Gava', 'checkout_label' => __('Pay with EcoCash or TeleCash', 'gava_edd'));
+function gava_edd_register_gateway($gateways)
+{
+	$gateways['gava_edd'] = array(
+		'admin_label' => 'Gava',
+		'checkout_label' => __('Pay with EcoCash or TeleCash', 'gava_edd')
+	);
+
 	return $gateways;
 }
 
 add_filter('edd_payment_gateways', 'gava_edd_register_gateway');
 
-function gava_edd_gateway_cc_form(){
+function gava_edd_gateway_cc_form()
+{
 	return;
 }
 
 add_action('edd_gava_edd_cc_form', 'gava_edd_gateway_cc_form');
 
-function gava_edd_settings($settings){
+function gava_edd_settings($settings)
+{
 	$s = array(
 			array(
 				'id' 	=> 'gava_edd_settings_h',
@@ -99,9 +106,7 @@ function gava_process_payment($purchase_data)
 			'cancel_url' => $return_url,			
 		);
 
-		$hash = $payload['reference'] . $payload['amount'] . $payload['return_url'] . $payload['cancel_url'];
-		$hash = hash('sha512', $hash . $edd_options['gava_secret_key']);
-		$payload['signature'] = $hash;
+		$payload['signature'] = gava_sign($payload);
 
 		//create checkout, redirect to it
 		$endpoint = rtrim($edd_options['gava_checkout_url'], '/') . '/create?return_checkout_url';
@@ -127,6 +132,12 @@ function gava_process_payment($purchase_data)
 
 add_action('edd_gateway_gava_edd', 'gava_process_payment');
 
+/**
+ * Exit with error during checkout
+ *
+ * @param mixed $error Error
+ * @return void
+ */
 function gava_exit_with_error($error)
 {
 	global $edd_options;
@@ -145,7 +156,13 @@ function gava_exit_with_error($error)
 	exit();
 }
 
-
+/**
+ * Checks if two numerics are equal
+ *
+ * @param mixed $a First one
+ * @param mixed $b First one
+ * @return bool
+ */
 function gava_equal_floats($a, $b)
 {
 	$a = (float)$a;
@@ -154,16 +171,209 @@ function gava_equal_floats($a, $b)
 	if (abs(($a-$b)/$b) < 0.00001) {
 		return TRUE;
 	}
-	return FALSE;
 
+	return FALSE;
+}
+
+/**
+ * Exit with error
+ *
+ * @todo 	HTTP Status
+ * @param 	string $error Error
+ * @return 	void
+ */
+function gava_callback_error($error)
+{
+	echo $error;
+	die();
+}
+
+/**
+ * Fetches checkout with given hash.
+ * A return of false generally means the checkout is not valid to us
+ * Will exit with error for the ither scenarios
+ *
+ * @param string $hash Checkout hash
+ * return object|false
+ */
+function gava_fetch_checkout($hash)
+{
+	global $edd_options;
+
+	//Get checkout, confirm sig
+
+	$endpoint = rtrim($edd_options['gava_checkout_url'], '/') . '/checkout/details/' . $hash;
+
+	$response = wp_remote_get($endpoint);
+
+	if (is_wp_error($response)) {
+		return false;
+	}
+
+	$responseCode = wp_remote_retrieve_response_code($response);
+
+	if ($responseCode !== 200) {
+		gava_callback_error('Non-200 status during checkout fetch');
+	}
+
+	$checkout = json_decode(wp_remote_retrieve_body($response));
+
+	if (!$checkout) return false;
+
+	$expectedProperties = array(
+		'checkoutId',
+		'checkoutHash',
+		'reference',
+		'paid',
+		'amount',
+		'phone',
+		'transactionCode',
+		'paymentMethod',
+		'note',
+		'signature'
+	);
+
+	foreach ($expectedProperties as $property) {
+
+		if (!property_exists($checkout, $property)) return false;
+
+	}
+
+	if (!gava_validate_signature($checkout)) return false;
+
+	return $checkout;
 }
 
 
+/**
+ * Given a iterable $payload, it signs it with the secret key
+ *
+ * @param mixed $payload Object or array
+ * @return mixed
+ */
+function gava_sign($payload)
+{
+	global $edd_options;
+
+	$string = '';
+
+	foreach ($payload as $key => $value) {
+		if ($key === 'signature') continue;
+
+		$string .= $value;
+	}
+
+	return hash('sha512', $string . $edd_options['gava_secret_key']);
+}
+
+
+/**
+ * Given a iterable $payload, it signs it with the secret key
+ *
+ * @param mixed $payload Object or array
+ * @return mixed
+ */
+function gava_validate_signature($request)
+{
+	global $edd_options;
+
+	$string = '';
+
+	foreach ($request as $key => $value) {
+		if ($key === 'signature') continue;
+
+		$string .= $value;
+	}
+
+	$signature = hash('sha512', $string . $edd_options['gava_secret_key']);
+	return ($signature === $request->signature);
+}
+
+
+/**
+ * Listens for and processes Gava callabacks
+ *
+ * @return void
+ */
 function gava_edd_listen_for_callback()
 {
 	global $edd_options;
+
+	if (!isset($_GET['gava_callback'])) return;
 	
 	//Listen for callback, validate with server, close checkout
+	$callback = json_decode(file_get_contents('php://input'));
+
+	if (!$callback) gava_callback_error('Missing parameters');
+
+	$expectedProperties = array(
+		'checkoutId',
+		'checkoutHash',
+		'reference',
+		'paid',
+		'amount',
+		'phone',
+		'transactionCode',
+		'paymentMethod',
+		'note',
+		'signature'
+	);
+
+	foreach ($expectedProperties as $property) {
+
+		if (!property_exists($callback, $property)) gava_callback_error('Missing parameters');
+
+	}
+
+	if (!gava_validate_signature($callback))
+		gava_callback_error('Callback signature validation failed');
+
+	if (!$checkout = gava_fetch_checkout($callback->checkoutHash))
+		gava_callback_error('Checkout fetch failed');
+
+	//Defense: Gava doesn't yet have automated status changes from paid to not paid
+	if (!$checkout->paid) exit();
+
+	//Return silently if the payment cannot be found
+	if (!$payment = get_post($checkout->reference)) exit();
+	if ($payment->post_type !== 'edd_payment') exit();
+	if (edd_get_payment_gateway($checkout->reference) !== 'gava_edd') exit();
+
+	//Already published
+	if (get_post_status($checkout->reference) == 'publish') exit();
+
+	//I'm not sure under what conditions this would occur considering Gava checks that, but 
+	if (!gava_equal_floats($checkout->amount, edd_get_payment_amount($checkout->reference))) {
+
+		edd_record_gateway_error(
+			__( 'Amount mismatch error', 'gava_edd' ),
+			sprintf(
+				__( 'The amount paid (%s) and the amount expected are different', 'gava_edd'),
+				$checkout->amount
+			),
+			$checkout->reference
+		);
+
+		//Email admin
+		$body = "Customer paid $".$checkout->amount.". The order required ".$edd_get_payment_amount($checkout->reference);
+		$subject = '[Gava Easy Digital Downloads] Wrong settlement amount for order: '.$checkout->reference;
+		wp_mail(get_option('admin_email'), $subject, $body);
+		exit();
+	}
+
+	//We get this far, we can complete the order
+	edd_insert_payment_note(
+		$checkout->reference,
+		sprintf(
+			__('Gava Checkout ID: %s, Paid by: %s, transaction code: %s', 'gava_edd' ),
+			$checkout->id,
+			$checkout->phone,
+			$checkout->transactionCode
+		)
+	);
+
+	edd_update_payment_status($checkout->reference, 'publish');
+	exit();
 }
 
 add_action('init', 'gava_edd_listen_for_callback');
